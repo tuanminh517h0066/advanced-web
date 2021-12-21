80bdbe2153322a6fa2eea925c38f76096540a662
Your Name <you@example.com>
2021-12-21 16:45:56 +0700

    update notification

--- src/app/controllers/frontend/DepartmentController.js
+++ src/app/controllers/frontend/DepartmentController.js
@@ -13,6 +13,41 @@ class DepartmentController {
         });
     }
 
+    async departmentDetail(req, res, next) {
+
+        const current_account = req.user;
+
+        const department_item = await Department.findOne({_id: req.params.department_id})
+        .populate({
+            path: 'users',
+        });
+
+        res.render('frontend/department-item', {
+            department_item: mongooseToObject(department_item),
+            member: mongooseToObject(current_account),
+        });
+    }
+
+    async createNoti(req, res, next) {
+        
+        const department_item = await Department.findOne({_id: req.params.department_id})
+        const current_account = req.user;
+
+        res.render('frontend/create-notification', {
+            department_item: mongooseToObject(department_item),
+            member: mongooseToObject(current_account),
+        });
+    }
+
+    async ajaxUploadImageContent(req, res, next) {
+        var image='';
+        if(req.file) { 
+            // return response()->json(['success'=> 1, 'url' => '/uploads/clubs/img_content/' . $image]);
+            image = req.file.filename;
+        }
+        res.json({success: 1, url: '/uploads/'+ image});
+    }
+
 }
 
 module.exports = new DepartmentController;
--- src/routes/department.route.js
+++ src/routes/department.route.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const userMiddleware = require('../app/middleware/UserMiddleware');
 const DepartmentController = require('../app/controllers/frontend/DepartmentController');
 
+const multer  = require('multer')
+
+
+// const upload = multer({ dest: './src/public/uploads/' });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './src/public/uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
 router.get('/list', userMiddleware.isMember, DepartmentController.list );
+
+router.get('/:department_id',userMiddleware.isMember, DepartmentController.departmentDetail);
+
+router.get('/:department_id/notification/create', userMiddleware.isMember, DepartmentController.createNoti);
+
+router.post('/ajaxUploadImageContent', userMiddleware.isMember, upload.single('image'), DepartmentController.ajaxUploadImageContent);
 
 module.exports = router;
