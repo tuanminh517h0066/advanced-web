a6fcea7f8968797110a6e977d0d4d0b592072891
Toan Le <lnktoan1011@gmail.com>
2022-01-08 23:11:35 +0700

    push host

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/final_project');
+        await mongoose.connect('mongodb+srv://admin:<password>@cluster0.wvmvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
         console.log("connect successfully!!!");
     } catch (error) {
